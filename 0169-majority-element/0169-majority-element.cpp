bd71be6c9cac55368c992f714e0381c25865b68c
Rohit Sahay <101516312+rohitsahayy@users.noreply.github.com>
2024-05-31 12:45:11 +0530

    Time: 14 ms (81.94%), Space: 26.7 MB (70.64%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        /* O(N) :SC
         unordered_map<int,int> mp1;
         for(auto num:nums){
             mp1[num]++;
@@ -15,5 +16,26 @@ public:
         }
     }
     return majority;
+    */
+
+    //For O(1) SC:
+
+    int majority = nums[0];
+    int vote = 1;
+    for(int i=1;i<nums.size();i++){
+        if(vote==0){
+            majority=nums[i];
+            vote=1;
+        }
+        else if(majority==nums[i]){
+            vote++;
+        }
+        else{
+            vote--;
+        }
+    }
+
+    return majority;
+
     }
 };
